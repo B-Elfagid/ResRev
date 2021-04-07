5f7f1d7d8989f29ef38ab1f031d21c482b9578c5
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-04-08 00:15:53 +0100

    add forms

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,28 @@
 class OwnersController < ApplicationController
+    def new
+        @owner = Owner.new
+    end 
+    
+    def create
+      @owner = Owner.new(owner_params)
+      if @owner.save
+        session[:owner_id] = @owner.id
+        redirect_to new_restaurant_path
+      else 
+        redirect_to :'/'
+    end 
+ end  
+
+    def show
+        @owner = Owner.find_by(id: params[:id])
+        redirect_to '/' if !@owner
+    end 
+
+
+    private 
+
+    def owner_params
+        params.require(:owner).permit(:first_name, :last_name, :email, :password)
+    end 
+
 end
