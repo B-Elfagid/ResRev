0bc70fc6315793a9c72d8114d225566b5d592766
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-04-27 22:24:16 +0100

    add image for review page and adjust size

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-     has_many :restaurants
-     has_many :reviews
-     has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+    has_many :reviews
+    has_many :restaurants
+    has_many :reviewed_restaurants, through: :reviews, source: :restaurant
 
    
 
