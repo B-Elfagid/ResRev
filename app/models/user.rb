fae2ba691731a3ee3b81138edcde642f0344effb
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-04-28 00:27:34 +0100

    add condition for only current_user to edit/delete reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :restaurants
-    has_many :reviewed_restaurants, through: :reviews, source: :restaurant
-
+    has_many :restaurants, through: :reviews 
    
 
     has_secure_password 
